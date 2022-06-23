3c3011325cbd45b8dff5058f9dcabd2388027bd3
Scott Lau <exceedego@126.com>
2022-06-23 09:46:20 +0800

    列索引自动减一无须再手工减一

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'sc-utilities>=0.0.8',
+        'sc-utilities>=0.0.9',
     ],
     package_data={
         'sc_excel_column_calculator': ['tests/sample_config/*'],
